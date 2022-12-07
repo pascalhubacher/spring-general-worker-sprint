c1ad71a3da5cb48c719b5a57fd7560af4f0d3030
Pascal Hubacher <pascal.hubacher@outlook.com>
2022-12-07 21:54:30 +0100

    initial setup

---
+++ src/main/java/com/mas2022datascience/springgeneralworkersprint/SpringGeneralWorkerSprintApplication.java
@@ -0,0 +1,13 @@
+package com.mas2022datascience.springgeneralworkersprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGeneralWorkerSprintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGeneralWorkerSprintApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/mas2022datascience/springgeneralworkersprint/admin/Topics.java
@@ -0,0 +1,46 @@
+package com.mas2022datascience.springgeneralworkersprint.admin;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.common.config.TopicConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Topics {
+
+  @Value(value = "${topic.tracab-01.name}")
+  private String topicName1;
+  @Value(value = "${topic.tracab-01.partitions}")
+  private Integer topicPartitions1;
+  @Value(value = "${topic.tracab-01.replication-factor}")
+  private Integer topicReplicationFactor1;
+
+  // creates or alters the topic
+  @Bean
+  public NewTopic tracab01() {
+    return TopicBuilder.name(topicName1)
+        .partitions(topicPartitions1)
+        .replicas(topicReplicationFactor1)
+        .config(TopicConfig.RETENTION_MS_CONFIG, "-1")
+        .build();
+  }
+
+  @Value(value = "${topic.tracab-02.name}")
+  private String topicName2;
+  @Value(value = "${topic.tracab-02.partitions}")
+  private Integer topicPartitions2;
+  @Value(value = "${topic.tracab-02.replication-factor}")
+  private Integer topicReplicationFactor2;
+
+  // creates or alters the topic
+  @Bean
+  public NewTopic tracab02() {
+    return TopicBuilder.name(topicName2)
+        .partitions(topicPartitions2)
+        .replicas(topicReplicationFactor2)
+        .config(TopicConfig.RETENTION_MS_CONFIG, "-1")
+        .build();
+  }
+}
